8b980b87349bda7906e7030dae38322efefa8df7
Nick Jennings <nije@microsoft.com>
2023-12-13 01:24:28 -0800

    Day 6 parts 1 and 2

---
+++ Projects/6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace AoC2023_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLines = File.ReadAllLines(".\\Input.txt");
+            List<TimeDistancePair> pairs = new List<TimeDistancePair>();
+
+            var times = inputLines[0].Split(':')[1].Split(' ').Where(x => x != string.Empty).ToList();
+            var distances = inputLines[1].Split(':')[1].Split(' ').Where(x => x != string.Empty).ToList();
+
+            var combinedTime = new StringBuilder();
+            var combinedDistance = new StringBuilder();
+
+            for (int x = 0; x < times.Count; x++)
+            {
+                var pair = new TimeDistancePair()
+                {
+                    Time = int.Parse(times[x]),
+                    Distance = int.Parse(distances[x])
+                };
+
+                combinedTime.Append(times[x]);
+                combinedDistance.Append(distances[x]);
+
+                pairs.Add(pair);
+            }
+
+            var giantPair = new TimeDistancePair()
+            {
+                Time = long.Parse(combinedTime.ToString()),
+                Distance = long.Parse(combinedDistance.ToString())
+            };
+
+            long combinations = 1;
+
+            foreach (var pair in pairs)
+            {
+                combinations *= pair.NumberOfWaysToWin();
+            }
+
+            Console.WriteLine($"Part 1: {combinations}");
+            Console.WriteLine($"Part 2: {giantPair.NumberOfWaysToWin()}");
+        }
+    }
+}
---
+++ Projects/6/TimeDistancePair.cs
@@ -0,0 +1,26 @@
+﻿namespace AoC2023_6
+{
+    public class TimeDistancePair
+    {
+        public long Time;
+        public long Distance;
+
+        public TimeDistancePair()
+        {
+        }
+
+        public long NumberOfWaysToWin()
+        {
+            long numberOfWins = 0;
+            for (long i = 1; i < Time; i++)
+            {
+                if ((Time - i) * i > Distance)
+                {
+                    numberOfWins++;
+                }
+            }
+
+            return numberOfWins;
+        }
+    }
+}
