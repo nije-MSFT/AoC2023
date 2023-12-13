0fa934cae55a8c7da241a190147fe250cb49e520
Nick Jennings <nije@microsoft.com>
2023-12-12 19:07:29 -0800

    Day 4 part 1

---
+++ Projects/4/Card.cs
@@ -0,0 +1,27 @@
+﻿namespace AoC2023_4
+{
+    internal class Card
+    {
+        public int CardNumber { get; }
+        
+        public Dictionary<int, bool> WinningNumbers { get; }
+        
+        public Card(int cardNumber, IEnumerable<int> winningNumbers)
+        {
+            CardNumber = cardNumber;
+            WinningNumbers = winningNumbers.ToDictionary(x => x, x => true);
+        }
+
+        public int GetCardValue(IEnumerable<int> playingNumbers)
+        {
+            var numberOfWins = playingNumbers.Count(x => WinningNumbers.GetValueOrDefault(x));
+
+            if (numberOfWins > 0) 
+            {
+                return 1 << numberOfWins - 1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Projects/4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AoC2023_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLines = File.ReadAllLines(".\\Input.txt");
+
+            var totalPart1 = 0;
+            var totalPart2 = 0;
+
+            foreach (var line in inputLines)
+            {
+                var cardNumberAndOtherNumbers = line.Split(':');
+                
+                var playerNumbersAndWinningNumbers = cardNumberAndOtherNumbers[1].Split('|');
+                
+                var card = new Card(
+                    int.Parse(cardNumberAndOtherNumbers[0].Split(' ').Last()),
+                    playerNumbersAndWinningNumbers[0].Split(' ').Where(x => x != string.Empty).Select(x => int.Parse(x)));
+
+                totalPart1 += card.GetCardValue(playerNumbersAndWinningNumbers[1].Split(' ').Where(x => x != string.Empty).Select(x => int.Parse(x)));
+            }
+
+            Console.WriteLine($"Part 1: {totalPart1}");
+            Console.WriteLine($"Part 2: {totalPart2}");
+        }
+    }
+}
