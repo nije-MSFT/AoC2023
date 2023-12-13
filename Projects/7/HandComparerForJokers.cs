4fb0a700ce54afd7cae8649e890fcc200b951cbe
Nick Jennings <nije@microsoft.com>
2023-12-13 04:22:45 -0800

    Day 7 part 2

--- Projects/7/HandComparerForJokers.cs
+++ Projects/7/HandComparerForJokers.cs
@@ -1,25 +1,25 @@
 ﻿namespace AoC2023_7
 {
-    public class HandComparer : IComparer<Hand>
+    public class HandComparerForJokers : IComparer<Hand>
     {
         private Dictionary<char, int> CardValues = new Dictionary<char, int>()
         {
             {'A', 13 },
             {'K', 12 },
             {'Q', 11 },
-            {'J', 10 },
-            {'T', 9 },
-            {'9', 8 },
-            {'8', 7 },
-            {'7', 6 },
-            {'6', 5 },
-            {'5', 4 },
-            {'4', 3 },
-            {'3', 2 },
-            {'2', 1 },
+            {'T', 10 },
+            {'9', 9 },
+            {'8', 8 },
+            {'7', 7 },
+            {'6', 6 },
+            {'5', 5 },
+            {'4', 4 },
+            {'3', 3 },
+            {'2', 2 },
+            {'J', 1 },
         };
 
-        public HandComparer()
+        public HandComparerForJokers()
         {
 
         }
@@ -59,23 +59,34 @@
                 .GroupBy(x => x)
                 .ToDictionary(y => y.Key, y => y.Count());
 
-            if (cardCounts.Keys.Count == 1)
+            if (!cardCounts.Keys.Contains('J'))
+            {
+                return HandComparer.DetermineHandType(cards);
+            }
+
+            if  (cardCounts.Keys.Count <= 2)
             {
                 return HandType.FiveOfAKind;
             }
-            if (cardCounts.Keys.Count == 2)
+
+            if (cardCounts.Keys.Count == 3)
             {
-                return cardCounts.Values.Max() == 4 ?
+                return cardCounts.Values.Max() + cardCounts['J'] >= 4 ?
                     HandType.FourOfAKind :
                     HandType.FullHouse;
             }
+
+            if (cardCounts.Keys.Count == 4)
+            {
+                return HandType.ThreeOfAKind;
+            }
+
             if (cardCounts.Keys.Count == 3)
             {
-                return cardCounts.Values.Max() == 3 ?
-                    HandType.ThreeOfAKind :
-                    HandType.TwoPair;
+                return HandType.TwoPair;
             }
-            if (cardCounts.Keys.Count == 4)
+
+            if (cardCounts.Keys.Count == 5)
             {
                 return HandType.OnePair;
             }
--- Projects/7/Program.cs
+++ Projects/7/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2023_7
+﻿using System.Diagnostics;
+
+namespace AoC2023_7
 {
     internal class Program
     {
@@ -8,6 +10,8 @@
             var totalPart2 = 0;
 
             var inputLines = File.ReadAllLines(".\\Input.txt");
+
+            var timer = Stopwatch.StartNew();
 
             var allHands = new List<Hand>();
 
@@ -23,6 +27,14 @@
                 totalPart1 += allRankedHands[i].Wager * (allRankedHands.Length - i);
             }
 
+            allRankedHands = allHands.OrderByDescending(x => x, new HandComparerForJokers()).ToArray();
+
+            for (int i = 0; i < allRankedHands.Length; i++)
+            {
+                totalPart2 += allRankedHands[i].Wager * (allRankedHands.Length - i);
+            }
+
+            timer.Stop();
             Console.WriteLine($"Part 1: {totalPart1}");
             Console.WriteLine($"Part 2: {totalPart2}");
         }
